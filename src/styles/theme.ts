f022994816402620e4759fe6ec7cf8e0fc4fb6f0
karczewskiPiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2021-08-05 11:17:20 +0200

    Include typography in the theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,6 +16,19 @@ const theme = {
       primary: "'HK-Grotesk', sans-serif",
       secondary: "'CrimsonPro', serif",
     },
+    desktop: {
+      xl: "60px",
+      lg: "25px",
+      md: "20px",
+      sm: "13px",
+    },
+    mobile: {
+      xxl: "48px",
+      xl: "32px",
+      lg: "22px",
+      md: "18px",
+      sm: "13px",
+    },
   },
 } as const
 
