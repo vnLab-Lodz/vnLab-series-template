74f1c2a1cad5f03a4d1a2f853afe4eebc58a68ee
Piotr Karczewski <p.karczewsk@gmail.com>
2021-08-05 10:45:36 +0200

    Include global styles with css reset

--- config/gatsby-browser.tsx
+++ config/gatsby-browser.tsx
@@ -2,7 +2,11 @@ import { WrapRootElementNodeArgs } from "gatsby"
 import { ThemeProvider } from "styled-components"
 import theme from "../src/styles/theme"
 import React from "react"
+import { Globals } from "../src/styles/globals"
 
 export const wrapRootElement = ({ element }: WrapRootElementNodeArgs) => (
-  <ThemeProvider theme={theme}>{element}</ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <Globals />
+    {element}
+  </ThemeProvider>
 )
