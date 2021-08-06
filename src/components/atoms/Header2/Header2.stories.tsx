d603568a5088d1f744c4bd6cf429b0cde3559e15
karczewskiPiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2021-08-06 13:16:40 +0200

    Alter h2 story

--- src/components/atoms/Header2/Header2.stories.tsx
+++ src/components/atoms/Header2/Header2.stories.tsx
@@ -7,20 +7,20 @@ export default {
   title: "Atoms/Header 2",
 } as Meta
 
-const Header1Story: Story = args => <H2 {...args} />
+const H2Story: Story = args => <H2 {...args} />
 
-export const Default = Header1Story.bind({})
+export const Default = H2Story.bind({})
 Default.args = {
   children: "Example Header 2",
 }
 
-export const CrimsonPro = Header1Story.bind({})
+export const CrimsonPro = H2Story.bind({})
 CrimsonPro.args = {
   children: "Crimson Pro Header 2",
   type: "secondary",
 }
 
-export const HKGrotesk = Header1Story.bind({})
+export const HKGrotesk = H2Story.bind({})
 HKGrotesk.args = {
   children: "HK-Grotesk Header 2",
   type: "primary",
