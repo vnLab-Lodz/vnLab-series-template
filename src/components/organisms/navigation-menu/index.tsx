812f69134899e59eaff96dc61b566d1ccf14d95e
Piotr Karczewski <p.karczewsk@gmail.com>
2021-08-26 16:43:00 +0200

    Display localized title on nav menu

--- src/components/organisms/navigation-menu/index.tsx
+++ src/components/organisms/navigation-menu/index.tsx
@@ -1,20 +1,18 @@
 import React, { useContext } from "react"
+import { NavMenuContext } from "./nav-menu-context"
+import { useTranslation } from "react-i18next"
 import * as Styled from "./style"
-import { graphql, useStaticQuery } from "gatsby"
 
 //@ts-ignore
 import HamburgerSVG from "../../../images/icons/hamburger.svg"
 //@ts-ignore
 import VnlabLogo from "../../../images/icons/vnlab_logo.svg"
-import { NavMenuContext } from "./nav-menu-context"
 
 interface Props {}
 
-// const query = graphql``
-
 const NavigationMenu: React.FC<Props> = () => {
-  // const data = useStaticQuery()
   const { navMode } = useContext(NavMenuContext)
+  const { t } = useTranslation("common")
 
   return (
     <Styled.Aside>
@@ -22,7 +20,7 @@ const NavigationMenu: React.FC<Props> = () => {
         <Styled.HamburgerBtn onClick={() => alert("Click")}>
           <img src={HamburgerSVG} alt="Hamburger Button" />
         </Styled.HamburgerBtn>
-        <Styled.Title>Nowe narracje wizualne</Styled.Title>
+        <Styled.Title>{t("title")}</Styled.Title>
         <img src={VnlabLogo} alt="vnLab logo" />
       </Styled.Nav>
     </Styled.Aside>
