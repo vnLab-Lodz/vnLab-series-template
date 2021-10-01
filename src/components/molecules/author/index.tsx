e59948eb2f305ac5c7cd13b6b0ce0478c7547668
Piotr Karczewski <p.karczewsk@gmail.com>
2021-10-01 15:14:35 +0200

    Allow for links in article author

--- src/components/molecules/author/index.tsx
+++ src/components/molecules/author/index.tsx
@@ -1,8 +1,25 @@
+import { useLocalization } from "gatsby-theme-i18n"
 import React from "react"
 import * as Styled from "./style"
 
-const Author: React.FC = ({ children }) => {
-  return <Styled.H3 type="primary">{children}</Styled.H3>
+interface Props {
+  link?: string
+}
+
+const Author: React.FC<Props> = ({ children, link }) => {
+  const { locale } = useLocalization()
+
+  return (
+    <Styled.H3 type="primary">
+      {link ? (
+        <Styled.BiogramLink to={link} lang={locale}>
+          {children}
+        </Styled.BiogramLink>
+      ) : (
+        children
+      )}
+    </Styled.H3>
+  )
 }
 
 export default Author
--- src/components/molecules/author/style.ts
+++ src/components/molecules/author/style.ts
@@ -1,7 +1,16 @@
+import { LocalizedLink } from "gatsby-theme-i18n"
 import styled from "styled-components"
 import atoms from "~components/atoms"
 
 export const H3 = styled(atoms.h3)`
   margin-bottom: ${({ theme: { spacing } }) => spacing.lg};
   text-align: start;
+`
+
+export const BiogramLink = styled(LocalizedLink)`
+  text-decoration: none;
+  color: inherit;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
 `
