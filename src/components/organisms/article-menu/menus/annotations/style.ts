1eda4818f968d54a26928e4e56e66ebefd3a8ba7
Piotr Karczewski <p.karczewsk@gmail.com>
2021-11-30 12:53:43 +0100

    style: :lipstick: Expand footnote index space on mobile

--- src/components/organisms/article-menu/menus/annotations/style.ts
+++ src/components/organisms/article-menu/menus/annotations/style.ts
@@ -1,18 +1,27 @@
 import { InnerGrid } from "~styles/grid"
 import styled from "styled-components"
 import atoms from "~components/atoms"
+import { devices } from "~styles/breakpoints"
 
 export const AnnotationsGrid = styled(InnerGrid)`
   row-gap: ${({ theme: { spacing } }) => spacing.xs};
 `
 
 export const AnnotationNumber = styled(atoms.p)`
-  grid-column: 1;
+  grid-column: 1 / 2;
+
+  @media ${devices.tablet} {
+    grid-column: 1;
+  }
 `
 
 export const AnnotationParagraph = styled(atoms.p)`
   cursor: pointer;
-  grid-column: 2 / last-col;
+  grid-column: 3 / last-col;
+
+  @media ${devices.tablet} {
+    grid-column: 2 / last-col;
+  }
 `
 
 export const InheritParagraph = styled.p`
