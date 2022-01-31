2f63bf519edc28c4fabd06cfd4155a7d2d9ac5f7
Piotr Karczewski <p.karczewsk@gmail.com>
2022-01-31 15:01:55 +0100

    fix: :green_heart: Remove window usage from top level hook

--- src/components/molecules/slides/two-image-slide/index.tsx
+++ src/components/molecules/slides/two-image-slide/index.tsx
@@ -1,5 +1,6 @@
 import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image"
 import React, { useEffect, useState } from "react"
+import useScreenDimensions from "src/hooks/useScreenDimensions"
 import { useTheme } from "styled-components"
 import * as Styled from "./style"
 
@@ -8,23 +9,6 @@ interface Props {
   image2: IGatsbyImageData
   background?: string
   direction: "column" | "row"
-}
-
-const useScreenDimensions = () => {
-  const [width, setWidth] = useState(window.innerWidth)
-  const [height, setHeight] = useState(window.innerHeight)
-
-  const handleResize = () => {
-    setWidth(window.innerWidth)
-    setHeight(window.innerHeight)
-  }
-
-  React.useEffect(() => {
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
-
-  return { width, height }
 }
 
 const TwoImageSlide: React.FC<Props> = ({
