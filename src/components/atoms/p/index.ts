296f96ed447cc0ec912a6a5d98687d49c2e0b5fe
Piotr Karczewski <p.karczewsk@gmail.com>
2022-02-16 18:31:12 +0100

    style: :lipstick: Change weight of Crimson Pro to light where it was normal before

--- src/components/atoms/p/index.ts
+++ src/components/atoms/p/index.ts
@@ -1,11 +1,10 @@
 import styled, { css } from "styled-components"
-import { devices } from "../../../styles/breakpoints"
 
 export const p = styled.p`
   ${({ theme: { typography, palette } }) => css`
     font-family: ${typography.fonts.secondary};
     font-size: ${typography.md};
-    font-weight: normal;
+    font-weight: 300;
     font-style: normal;
     line-height: 150%;
     color: ${palette.black};
