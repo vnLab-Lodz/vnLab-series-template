15896e579818cd9a88877e0c0db1754893676f60
Piotr Karczewski <p.karczewsk@gmail.com>
2022-06-28 07:54:04 +0200

    feat: :label: Update SVG type definitions

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,11 +1,6 @@
 import "styled-components"
 import { Theme } from "../styles/theme"
 
-declare module "*.svg" {
-  const content: any
-  export default content
-}
-
 declare module "styled-components" {
   export interface DefaultTheme extends Theme {}
 }
