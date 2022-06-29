a7480c428f579e39a95c3b2ed5d56a720507c3a7
Piotr Karczewski <p.karczewsk@gmail.com>
2022-06-29 17:15:03 +0200

    feat: :sparkles: Use font-display swap for fonts

--- config/gatsby-config.ts
+++ config/gatsby-config.ts
@@ -72,7 +72,7 @@ export default {
           urls: [`/fonts/fonts.css`],
         },
         google: {
-          families: [`Crimson Pro:300,400,500,600,700,800,900`],
+          families: [`Crimson Pro:300,400,500,600,700,800,900&display=swap`],
         },
       },
     },
