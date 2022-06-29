51682404be0a7ad125f6fca9f5bd58d20051e9ac
Piotr Karczewski <p.karczewsk@gmail.com>
2022-06-29 20:18:14 +0200

    feat: :package: Update gatsby and use Script component for loading hypothesis

--- config/gatsby-browser.tsx
+++ config/gatsby-browser.tsx
@@ -1,4 +1,4 @@
-import { WrapPageElementBrowserArgs } from "gatsby"
+import { WrapPageElementBrowserArgs, Script, ScriptStrategy } from "gatsby"
 import { ThemeProvider } from "styled-components"
 import { darkTheme, lightTheme } from "../src/styles/theme"
 import React from "react"
@@ -14,18 +14,24 @@ import {
 export const wrapPageElement = ({ element }: WrapPageElementBrowserArgs) => (
   <ThemeSwitcherProvider>
     {({ themeMode }: { themeMode: THEME_MODES }) => (
-      <ThemeProvider
-        theme={themeMode === THEME_MODES.LIGHT ? lightTheme : darkTheme}
-      >
-        <HypothesisContextProvider>
-          <HypothesisManager>
-            <ScrollContextProvider>
-              <Globals />
-              {element}
-            </ScrollContextProvider>
-          </HypothesisManager>
-        </HypothesisContextProvider>
-      </ThemeProvider>
+      <>
+        <ThemeProvider
+          theme={themeMode === THEME_MODES.LIGHT ? lightTheme : darkTheme}
+        >
+          <HypothesisContextProvider>
+            <HypothesisManager>
+              <ScrollContextProvider>
+                <Globals />
+                {element}
+              </ScrollContextProvider>
+            </HypothesisManager>
+          </HypothesisContextProvider>
+        </ThemeProvider>
+        <Script
+          src="https://hypothes.is/embed.js"
+          strategy={ScriptStrategy.postHydrate}
+        />
+      </>
     )}
   </ThemeSwitcherProvider>
 )
--- config/gatsby-ssr.tsx
+++ config/gatsby-ssr.tsx
@@ -1,10 +1,1 @@
-import { RenderBodyArgs } from "gatsby"
-import React from "react"
-
-export const onRenderBody = ({ setPostBodyComponents }: RenderBodyArgs) => {
-  setPostBodyComponents([
-    <script key="hypothesis-embed" src="https://hypothes.is/embed.js" async />,
-  ])
-}
-
 export { wrapPageElement } from "./gatsby-browser"
