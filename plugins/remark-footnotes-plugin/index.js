c2cd36def4b30ab5cdd6b683332a4cdda89d3b8c
Piotr Karczewski <p.karczewsk@gmail.com>
2022-12-12 16:09:47 +0100

    feat: :sparkles: Add remark-footnotes-plugin to template

---
+++ plugins/remark-footnotes-plugin/index.js
@@ -0,0 +1,133 @@
+const visit = require(`unist-util-visit`)
+const toString = require(`mdast-util-to-string`)
+const u = require("unist-builder")
+const remove = require("unist-util-remove")
+import { v4 as nanoid } from "uuid"
+
+const visitTypes = [
+  "heading",
+  "paragraph",
+  "blockquote",
+  "list",
+  "listItem",
+  "emphasis",
+  "strong",
+  "link",
+  "linkReference",
+]
+
+// ? Could be interesting to look at this ↓
+// ? https://gitlab.com/staltz/unist-util-flatmap
+function traverse(node, footnotesNodes, util) {
+  const textIndexes = []
+
+  node.children.forEach((child, i) => {
+    if (child.type === "text") textIndexes.push(i)
+  })
+
+  if (textIndexes.length === 0) return
+
+  for (let i = textIndexes.length - 1; i >= 0; i--) {
+    const index = textIndexes[i]
+    const childNode = node.children[index]
+
+    const newChildren = constructNewChildren(childNode, util, footnotesNodes)
+    if (newChildren.length < 3) continue
+
+    node.children.splice(index, 1, ...newChildren)
+  }
+}
+
+function constructNewChildren(textNode, util, footnotesNodes, children = []) {
+  const regexp = new RegExp(
+    /(\([^\)]*\)|[\S.]*|\({2}[^\)]*\)[^\)]*\){1}):{{\^\d*}}/,
+    "g"
+  )
+  const match = regexp.exec(textNode.value)
+  if (match === null) return children.length > 0 ? children : [textNode]
+
+  const [rawWord, rawFootnote] = match[0].split(":{{^")
+  const word = rawWord.startsWith("((") ? rawWord.slice(1, -1) : rawWord
+  const footnote = rawFootnote.slice(0, -2)
+  const parts = textNode.value.split(match[0])
+  if (parts.length > 2) {
+    throw new Error("There might be two of the same footnote in the text")
+  }
+
+  footnotesNodes.push({ word, footnote, content: "", uid: nanoid() })
+
+  const newChildren = [
+    u("text", parts[0]),
+    u(
+      "jsx",
+      `<FootnoteTarget id="footnote__anchor--${footnote}" to="#footnote--${footnote}">`
+    ),
+    u("text", word),
+    u("jsx", `<FootnoteIndex>${footnote}</FootnoteIndex></FootnoteTarget>`),
+    u("text", parts[1]),
+  ]
+
+  if (children.length > 0) {
+    children.splice(children.length - 1, 1, ...newChildren)
+  } else {
+    children.push(...newChildren)
+  }
+
+  return constructNewChildren(
+    children[children.length - 1],
+    util,
+    footnotesNodes,
+    children
+  )
+}
+
+function extractContents(node, footnotesNodes, util) {
+  if (node.lang !== "footnotes") return
+
+  const rawFootnotes = node.value.split("\n")
+
+  rawFootnotes.forEach(footnote => {
+    const raw = footnote.split("}}:")
+    const index = raw[0].slice(3)
+    const content = raw[1].trim()
+    const i = footnotesNodes.findIndex(n => n.footnote === index)
+    if (typeof i === "number") footnotesNodes[i].content = content
+  })
+}
+
+function createNodesFromFootnotes(footnotes, parent, util) {
+  footnotes.forEach(({ word, footnote, content: footnoteContent, uid }) => {
+    // TODO: Make unique across all files
+    const id = `footnote__${uid}--${footnote}`
+    const content = {
+      index: footnote,
+      content: footnoteContent,
+      target: word,
+      link: `#footnote--${footnote}`,
+    }
+    util.actions.createNode({
+      ...content,
+      id: util.createNodeId(`FOOTNOTES__${id}`),
+      children: [],
+      parent: parent.id,
+      internal: {
+        type: "Footnotes",
+        content: JSON.stringify(content),
+        contentDigest: util.createContentDigest(content),
+      },
+    })
+  })
+}
+
+function isFootnotes(node) {
+  return node.type === "code" && node.lang === "footnotes"
+}
+
+module.exports = ({ markdownAST: tree, ...util }, pluginOptions) => {
+  const footnotes = []
+  visit(tree, visitTypes, node => traverse(node, footnotes, util))
+  visit(tree, isFootnotes, node => extractContents(node, footnotes, util))
+  remove(tree, isFootnotes)
+  createNodesFromFootnotes(footnotes, util.markdownNode, util)
+  return tree
+}
