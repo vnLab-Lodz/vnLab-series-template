c530aa9c567a625986927b8a58f9bba1d6199e42
Piotr Karczewski <p.karczewsk@gmail.com>
2022-12-22 17:09:31 +0100

    feat: :lipstick: Implement new quote style

--- src/components/molecules/quote/index.tsx
+++ src/components/molecules/quote/index.tsx
@@ -1,4 +1,5 @@
 import React, { CSSProperties, PropsWithChildren } from "react"
+import { InnerGrid } from "~styles/grid"
 import * as Styled from "./style"
 
 interface Props {
@@ -18,6 +19,27 @@ const Quote: React.FC<PropsWithChildren<Props>> = ({
       <Styled.QuoteText>{children}</Styled.QuoteText>
       {author && <Styled.Author type="primary">{author}</Styled.Author>}
     </Styled.QuoteContainer>
+  )
+}
+
+export const BlockQuote: React.FC<PropsWithChildren<Props>> = ({
+  children,
+  author,
+  style,
+  className,
+}) => {
+  return (
+    <Styled.BlockQuoteContainer style={style} className={className}>
+      <Styled.BlockQuoteIcon role="img" aria-label="Quote symbol">
+        „
+      </Styled.BlockQuoteIcon>
+      <Styled.BlockQuoteText>{children}</Styled.BlockQuoteText>
+      {author && (
+        <Styled.BlockQuoteAuthor type="primary">
+          {author}
+        </Styled.BlockQuoteAuthor>
+      )}
+    </Styled.BlockQuoteContainer>
   )
 }
 
