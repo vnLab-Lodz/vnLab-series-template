a37a190577a35d917961a68a629c57902a7140d7
Piotr Karczewski <p.karczewsk@gmail.com>
2023-01-26 17:37:45 +0100

    feat: :sparkles: Add scrolling to carousel
    This commit includes some changes to viewport image slide and text slide styles as well.

--- src/components/molecules/slides/viewport-image-slide/style.ts
+++ src/components/molecules/slides/viewport-image-slide/style.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import {
   Absolute,
+  ImageWrapper,
   ViewportConstraint,
 } from "~components/molecules/viewport-image/style"
 import Slide from "../slide"
@@ -9,6 +10,14 @@ export const ViewportImageSlideContainer = styled(Slide)`
   & ${ViewportConstraint} {
     grid-template-rows: 0 1fr 0;
     max-height: 100vh;
+    margin: 0px !important;
+  }
+
+  & ${ImageWrapper} {
+    padding: 0;
+    margin-top: ${({ theme }) => theme.spacing.xs};
+
+    align-items: flex-end;
   }
 
   & ${Absolute} {
