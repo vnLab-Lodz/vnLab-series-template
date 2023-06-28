3cd8d4315332d28a5617b8ed8119f84f736e2dfd
Piotr Karczewski <p.karczewsk@gmail.com>
2023-06-28 10:13:04 +0100

    fix: :bug: Direct to proper tabs in NextTab

--- src/components/organisms/navigation-menu/components/next-tab.tsx
+++ src/components/organisms/navigation-menu/components/next-tab.tsx
@@ -1,25 +1,45 @@
-import React from "react"
+import React, { useCallback, useMemo } from "react"
 import { TFunction, useTranslation } from "react-i18next"
 import { NAV_MENU_STATES } from "../types"
 import * as Styled from "../style"
-
-export const nextTabNames = (t: TFunction<"nav-menu">) => ({
-  // ! Due to indxes disabling toc leads to about
-  [NAV_MENU_STATES.TOC]: t("tabs.indexes"),
-  [NAV_MENU_STATES.INDEXES]: t("tabs.about"),
-  [NAV_MENU_STATES.ABOUT]: t("tabs.toc"),
-})
-
-export const nextTabNavState = {
-  [NAV_MENU_STATES.TOC]: NAV_MENU_STATES.INDEXES,
-  [NAV_MENU_STATES.INDEXES]: NAV_MENU_STATES.ABOUT,
-  [NAV_MENU_STATES.ABOUT]: NAV_MENU_STATES.TOC,
-}
+import { useTags } from "../tabs/indexes"
 
 const NextTab: React.FC<{
   navState: NAV_MENU_STATES
   setNavState: React.Dispatch<React.SetStateAction<NAV_MENU_STATES>>
 }> = ({ navState, setNavState }) => {
+  const tags = useTags()
+
+  const nextTabNames = useCallback((t: TFunction<"nav-menu">) => {
+    if (tags.length === 0)
+      return {
+        [NAV_MENU_STATES.TOC]: t("tabs.about"),
+        [NAV_MENU_STATES.INDEXES]: t("tabs.about"),
+        [NAV_MENU_STATES.ABOUT]: t("tabs.toc"),
+      }
+
+    return {
+      [NAV_MENU_STATES.TOC]: t("tabs.indexes"),
+      [NAV_MENU_STATES.INDEXES]: t("tabs.about"),
+      [NAV_MENU_STATES.ABOUT]: t("tabs.toc"),
+    }
+  }, tags)
+
+  const nextTabNavState = useMemo(() => {
+    if (tags.length === 0)
+      return {
+        [NAV_MENU_STATES.TOC]: NAV_MENU_STATES.ABOUT,
+        [NAV_MENU_STATES.INDEXES]: NAV_MENU_STATES.ABOUT,
+        [NAV_MENU_STATES.ABOUT]: NAV_MENU_STATES.TOC,
+      }
+
+    return {
+      [NAV_MENU_STATES.TOC]: NAV_MENU_STATES.INDEXES,
+      [NAV_MENU_STATES.INDEXES]: NAV_MENU_STATES.ABOUT,
+      [NAV_MENU_STATES.ABOUT]: NAV_MENU_STATES.TOC,
+    }
+  }, tags)
+
   const { t } = useTranslation("nav-menu")
   const tabName = nextTabNames(t)[navState]
 
